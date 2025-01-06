c0947f3cf044ceaccc5b48c815ed3848cab29ef1
Rovembe <rovembekristoeffer.evsudc@gmail.com>
2025-01-07 00:47:00 +0800

    Added Bootstrap files and navbar

--- output/src/app/app.routes.ts
+++ output/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AnswersComponent } from './answers/answers.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'answers', component: AnswersComponent},
+    { path: 'reactive-form', component: ReactiveFormComponent}
+];
