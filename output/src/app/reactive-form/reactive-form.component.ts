f2be709327cf82e8a9d023001c6c915a519ecddd
Rovembe <rovembekristoeffer.evsudc@gmail.com>
2025-01-08 06:45:53 +0800

    Added Reactive Form (No validators yet)

--- output/src/app/reactive-form/reactive-form.component.ts
+++ output/src/app/reactive-form/reactive-form.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './reactive-form.component.html',
   styleUrl: './reactive-form.component.css'
 })
-export class ReactiveFormComponent {
+export class ReactiveFormComponent implements OnInit{
 
+  reactiveForm: FormGroup;
+
+  ngOnInit() {
+    this.reactiveForm = new FormGroup({
+      name: new FormControl(null, [Validators.required]),
+      lastname: new FormControl(null, [Validators.required]),
+      email: new FormControl(null, [Validators.required]),
+      mobile: new FormControl(null, [Validators.required]),
+      username: new FormControl(null, [Validators.required]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(12)]),
+    });
+  }
+
+  OnFormSubmitted(){
+    console.log(this.reactiveForm);
+    }
 }
